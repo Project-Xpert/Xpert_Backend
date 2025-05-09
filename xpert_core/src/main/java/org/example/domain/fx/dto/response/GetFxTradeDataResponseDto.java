75819d1472c494b49b7bcb9ac1540d119072f421
오송주 <dhthdwn7920@gmail.com>
2025-05-09 14:19:05 +0900

    fix: 추가적으로 필요한 데이터 DTO에 추가

--- xpert_core/src/main/java/org/example/domain/fx/dto/response/GetFxTradeDataResponseDto.java
+++ xpert_core/src/main/java/org/example/domain/fx/dto/response/GetFxTradeDataResponseDto.java
@@ -1,17 +1,21 @@
 package org.example.domain.fx.dto.response;
 
+import org.example.domain.fx.model.Fx;
 import org.example.domain.fx.model.FxData;
+import org.example.domain.fx.spi.vo.OwnFxVO;
 import org.example.domain.user.model.User;
 
 public record GetFxTradeDataResponseDto(
     int sellPrice,
     int buyPrice,
+    int ownedFx,
     Long userMoney
 ) {
-    public static GetFxTradeDataResponseDto of(User user, FxData fxData) {
+    public static GetFxTradeDataResponseDto of(User user, OwnFxVO ownedFx, FxData fxData) {
         return new GetFxTradeDataResponseDto(
                 fxData.getSellPrice(),
                 fxData.getBuyPrice(),
+                ownedFx.amount(),
                 user.getMoney()
         );
     }
--- xpert_core/src/main/java/org/example/domain/fx/usecase/GetFxTradeDataUseCase.java
+++ xpert_core/src/main/java/org/example/domain/fx/usecase/GetFxTradeDataUseCase.java
@@ -3,9 +3,12 @@ package org.example.domain.fx.usecase;
 import lombok.RequiredArgsConstructor;
 import org.example.common.service.CurrentUserProvider;
 import org.example.domain.fx.dto.response.GetFxTradeDataResponseDto;
+import org.example.domain.fx.model.Fx;
 import org.example.domain.fx.model.FxData;
 import org.example.domain.fx.model.FxType;
 import org.example.domain.fx.service.GetFxDataService;
+import org.example.domain.fx.service.GetFxService;
+import org.example.domain.fx.spi.vo.OwnFxVO;
 import org.example.domain.user.model.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,12 +19,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class GetFxTradeDataUseCase {
     private final CurrentUserProvider currentUserProvider;
     private final GetFxDataService getFxDataService;
+    private final GetFxService getFxService;
 
     public GetFxTradeDataResponseDto execute(FxType fxType) {
         User user = currentUserProvider.getCurrentUser();
 
         FxData fxData = getFxDataService.getNewestFxDataByFxType(fxType);
+        OwnFxVO ownedFx = getFxService.getFxOwnDataByUserAndFxType(user, fxType);
 
-        return GetFxTradeDataResponseDto.of(user, fxData);
+        return GetFxTradeDataResponseDto.of(user, ownedFx, fxData);
     }
 }
