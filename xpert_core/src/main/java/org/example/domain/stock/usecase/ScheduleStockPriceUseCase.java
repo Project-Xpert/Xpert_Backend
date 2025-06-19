74b3f79c57efff5488bd0fbf3b81f4d8131b595f
오송주 <dhthdwn7920@gmail.com>
2025-06-20 01:14:53 +0900

    fix: 주식 가격 불러오기 기능 1시간마다 업데이트 하도록 변경

--- xpert_core/src/main/java/org/example/domain/stock/usecase/ScheduleStockPriceUseCase.java
+++ xpert_core/src/main/java/org/example/domain/stock/usecase/ScheduleStockPriceUseCase.java
@@ -7,7 +7,6 @@ import org.example.domain.stock.modal.Stock;
 import org.example.domain.stock.modal.StockPrice;
 import org.example.domain.stock.service.CommandStockPriceService;
 import org.example.domain.stock.service.GetStockService;
-import org.example.domain.stock.spi.vo.StockCodeListVO;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +20,7 @@ public class ScheduleStockPriceUseCase {
     private final GetStockService getStockService;
     private final CommandStockPriceService commandStockPriceService;
 
-    @Scheduled(cron = "0 0/1 * 1/1 * ?")
+    @Scheduled(cron = "0 0 0/1 1/1 * ?")
     public void execute() {
         List<Stock> stockList = getStockService.getStocks();
 
