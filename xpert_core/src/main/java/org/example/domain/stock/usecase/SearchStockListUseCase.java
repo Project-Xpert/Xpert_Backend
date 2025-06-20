d06e3d5829fe13b1fdf0c4e27b3bfd424a8477e6
오송주 <dhthdwn7920@gmail.com>
2025-06-20 11:41:28 +0900

    fix: 한국 가격으로 반환하도록 코드 수정

--- xpert_core/src/main/java/org/example/domain/stock/usecase/SearchStockListUseCase.java
+++ xpert_core/src/main/java/org/example/domain/stock/usecase/SearchStockListUseCase.java
@@ -1,6 +1,7 @@
 package org.example.domain.stock.usecase;
 
 import lombok.RequiredArgsConstructor;
+import org.example.domain.fx.service.GetFxDataService;
 import org.example.domain.stock.dto.response.SearchStockListResponseDto;
 import org.example.domain.stock.dto.vo.StockOrderByEnum;
 import org.example.domain.stock.service.GetStockService;
@@ -13,9 +14,21 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SearchStockListUseCase {
     private final GetStockService getStockService;
+    private final GetFxDataService getFxDataService;
 
     public SearchStockListResponseDto execute(String keyword, StockOrderByEnum criteria) {
-        List<StockListItemVO> stockList = getStockService.searchStockByKeywordAndOrder(keyword, criteria);
+        int dollarPrice = getFxDataService.getTodayDollarPrice();
+
+        List<StockListItemVO> stockList = getStockService.searchStockByKeywordAndOrder(keyword, criteria)
+                .stream()
+                .map(data -> new StockListItemVO(
+                        data.stockId(),
+                        data.stockName(),
+                        Math.round(data.price() * dollarPrice),
+                        data.fluRate(),
+                        data.isBookmarked()
+                ))
+                .toList();
 
         return new SearchStockListResponseDto(stockList);
     }
